53e3fe341f981bfcfbc0774e22227246c0490778
Jesus Pichon <jesuspichonramirez@gmail.com>
2024-10-07 21:40:30 -0600

    tools added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const getProjects = async () => {
     try {
-        let requestJSON = await fetch('https://jesuspichon.github.io/portafolio/projects.json');
+        let requestJSON = await fetch('../projects.json');
         let response = await requestJSON.json();
 
         return response;
@@ -15,19 +15,29 @@ const showProjects = async () => {
         let projectHTML = '';
 
         projects.forEach(project => {
-            const { url, repository, image, name, description } = project;
+            const { url, repository, image, name, description, tools } = project;
+            let toolsHTML = '';
+
+            if (tools) {
+                tools.forEach(tool => {
+                    toolsHTML+=`<span class="badge rounded-pill text-bg-${tool.color} m-1">${tool.name}</span>`;
+                });
+            }
 
             projectHTML += `
                 <div class="col-12 col-sm-4">
-                    <div class="card m-2">
+                    <div class="card shadow-sm m-2">
                         <img src="${image}" alt="imagen proyecto">
                         <div class="card-body">
-                            <p class="text">${name}</p>
+                            <p class="h5 fw-bold">${name}</p>
                             <p class="text">${description}</p>
+                            <div class="d-flex">
+                                ${toolsHTML}
+                            </div>
                         </div>
                         <div class="card-footer d-flex justify-content-around">
-                            ${url ? `<a href="${url}" class="link-offset-2 link-underline link-underline-opacity-0">Ver</a>` : ''}
-                            ${repository ? `<a href="${repository}" class="link-offset-2 link-underline link-underline-opacity-0">GitHub</a>` : ''}
+                            ${url ? `<a href="${url}" class="link-offset-2 link-underline link-underline-opacity-0">Ver proyecto</a>` : ''}
+                            ${repository ? `<a href="${repository}" class="link-offset-2 link-underline link-underline-opacity-0">Repositorio</a>` : ''}
                         </div>
                     </div>
                 </div>
